754997a39e7bc96290b836235932fc5058083a79
DaBob77 <162922068+DaBob77@users.noreply.github.com>
2024-12-04 20:35:21 -0600

    Add files via upload

---
+++ Day One/Part 2.py
@@ -0,0 +1,25 @@
+list1 = []
+list2 = []
+file = open("input.txt", "r")
+lowestID1 = None
+lowestID2 = None
+output = 0
+
+for aline in file:
+    line = aline.split()
+    list1.append(line[0])
+    list2.append(line[1])
+
+
+list1.sort()
+list2.sort()
+
+for i in range(len(list1)):
+    instances = 0
+    for j in range(len(list2)):
+        if list1[i] == list2[j]:
+            instances += 1
+    output += int(list1[i]) * instances
+
+
+print(output)
