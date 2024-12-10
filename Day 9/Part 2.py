35df84b5a45054163f0393f8c811b32f8290d299
DaBob77 <162922068+DaBob77@users.noreply.github.com>
2024-12-10 16:30:23 +0000

    Day 9

--- Day 9/Part 2.py
+++ Day 9/Part 2.py
@@ -19,14 +19,22 @@ for i in range(len(input)):
             expanded.append(['.', input[i]])
 
 for i in range(len(expanded) - 1, 0, -1):
+    print(i)
     if expanded[i][0] != '.':
         for j in range(len(expanded)):
             if j < i:
-                if expanded[j][0] == '.' and expanded[i][1] == expanded[j][1]:
-                    print(expanded[i], expanded[j])
-                    expanded[j][0] = expanded[i][0]
-                    expanded[i][0] = "."
-                    break
+                if expanded[j][0] == '.':
+                    available = True
+                    for x in range(int(expanded[i][1])):
+                        if expanded[j + x][0] == '.':
+                            continue
+                        else:
+                            available = False
+                            break
+                    if available:
+                        for x in range(int(expanded[i][1])):
+                            expanded[j + x][0] = expanded[i - x][0]
+                            expanded[i - x][0] = "."
             else:
                 break
 
